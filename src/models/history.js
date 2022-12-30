d26d88c7679951baf6b0e137e232d4174a21bf28
Chirag Darbar <Chiragd@itpathsolutions.com>
2022-12-30 11:04:06 +0530

    history model

---
+++ src/models/history.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const historySchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    device_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Device",
+    },
+    pin_Id: {
+      type: String,
+    },
+    pin_Name: {
+      type: String,
+    },
+    switch_on_time: {
+      type: Date,
+      default: Date.now(),
+    },
+    switch_off_time: {
+      type: Date,
+    },
+    duration: {
+      type: String,
+      default: null,
+    },
+    cost: {
+      type: Number,
+      default: 0,
+    },
+    consumptionWattPrHr: {
+      type: String,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.export = mongoose.model("History", historySchema);
