dc3f3036ed4321e51b138767d602f8d986103c09
Chirag Darbar <Chiragd@itpathsolutions.com>
2022-12-30 12:30:35 +0530

    Partial commit.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,17 @@
-const express=require('express')
-const deviceController=require('../controllers/device.controller')
-const userController=require('../controllers/user.controller')
+const express = require("express");
+const deviceController = require("../controllers/device.controller");
+const userController = require("../controllers/user.controller");
+const historycontroller = require("../controllers/history.controller");
 
-const router=express.Router()
+const router = express.Router();
 
-router.post('/register/user',userController.createUser)
-router.post('/register/device',deviceController.createDevice)
+//User Controller
+router.post("/register/user", userController.createUser);
 
+//Device Controller
+router.post("/register/device", deviceController.createDevice);
 
-module.exports=router
+//History Controller
+router.get("/getUserHistory",historycontroller.getHistory);
+
+module.exports = router;
